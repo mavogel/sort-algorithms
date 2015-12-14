618d6b3468e4f39d8341272f910c9ef8c5041763
Manuel Vogel <mavogel@posteo.de>
2015-12-14 12:55:20 +0100

    fixed break issue in non optimal minsearch

--- algorithm_lab/minsearch.h
+++ algorithm_lab/minsearch.h
@@ -16,12 +16,8 @@ size_t optimalMinSearch(const std::array<T, SIZE>& array, const size_t startInde
     if(SIZE == 1) return 0;
 
     size_t minPos = startIndex;
-    T min = array[startIndex];
     for(size_t i = (startIndex + 1); i < endIndex; i++) {
-       if(array[i] < min) {
-           min = array[i];
-           minPos = i;
-       }
+       if(array[i] < array[minPos]) minPos = i;
     }
     return minPos;
 }
@@ -34,17 +30,12 @@ size_t optimalMinSearch(const std::array<T, SIZE>& array, const size_t startInde
  * @return the index of the minimum
  */
 template <typename T, size_t SIZE>
-size_t nonOptimalMinSearch(const std::array<T, SIZE>& array, const size_t startIndex = 0) {
+size_t nonOptimalMinSearch(const std::array<T, SIZE>& array, const size_t startIndex = 0, const size_t endIndex = SIZE) {
     if(SIZE == 1) return 0;
 
-    size_t minPos = (SIZE - 1);
-    T min = array[SIZE - 1];
-    for(size_t i = (SIZE - 2); i >= startIndex; i--) {
-       if(array[i] < min) {
-           min = array[i];
-           minPos = i;
-       }
-       if(i == startIndex) break;
+    size_t minPos = endIndex - 1;
+    for(size_t i = (endIndex - 1); i > startIndex;) {
+       if(array[--i] < array[minPos]) minPos = i;
     }
     return minPos;
 }
